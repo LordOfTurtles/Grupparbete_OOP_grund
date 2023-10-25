058fde131c1d6c5f4a514a910e3e3051b4ea55b6
Erik Johansson <erik.elias.johansson@gmail.com>
2023-10-25 13:10:44 +0200

    Testing new methods

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Hotel.AddRemoveRoom();
-        Hotel.AddRemoveRoom();
-        Hotel.AddRemoveRoom();
+        Hotel.Rooms.Add(new Room("1", "Royal suite", 4000, 5, 4));
+        Hotel.Rooms.Add(new Room("2", "Double room", 1500, 3, 2));
+        Hotel.Rooms.Add(new Room("3", "Single room", 600, 1, 3));
+        Hotel.Rooms.Add(new Room("4", "Family room", 1800, 5, 2));
+        Hotel.RoomAvaliability();
+        Hotel.CheckIn();
+        Hotel.RoomAvaliability();
+        Hotel.CheckOut();
+        Hotel.RoomAvaliability();
+
     }
 }
