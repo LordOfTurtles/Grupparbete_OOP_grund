63e083740d00b5ac1accf366e2fede88ac054ed0
Erik Johansson <erik.elias.johansson@gmail.com>
2023-10-25 13:10:11 +0200

    Added methods for checking in/out

--- hotel.cs
+++ hotel.cs
@@ -1,4 +1,6 @@
 
+using System.Security.Cryptography.X509Certificates;
+
 namespace Hotel_Management_Software;
 
 class Hotel
@@ -50,21 +52,69 @@ class Hotel
 
     }
 
-    static void CheckIn()
+    public static void CheckIn()
     {
 
+        Console.Write("What roomnumber is getting checked into?: ");
+        string roomNr = Console.ReadLine()!;
+        if(Rooms.Exists(x => x.RoomNr.Contains(roomNr)))
+        {
+            int i = Rooms.FindIndex(x => x.RoomNr.Contains(roomNr));
+            if(Rooms[i].isChecked == false)
+            {
+            Rooms[i].isChecked = true;
+            Console.WriteLine($"Room number: {Rooms[i].RoomNr}, {Rooms[i].Description}. has been checked into.");
+            }
+            else
+            {
+                Console.WriteLine($"Error, Room number: {Rooms[i].RoomNr}, {Rooms[i].Description}. is already checked into.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error! no room with roomnumber '{roomNr}' exists");
+        }
     }
 
-    static void  CheckOut()
+    public static void  CheckOut()
     {
-
+        Console.Write("What roomnumber is getting checked out of?: ");
+        string roomNr = Console.ReadLine()!;
+        if(Rooms.Exists(x => x.RoomNr.Contains(roomNr)))
+        {
+            int i = Rooms.FindIndex(x => x.RoomNr.Contains(roomNr));
+            if(Rooms[i].isChecked == true)
+            {
+                Rooms[i].isChecked = false;
+                Console.WriteLine($"Room number: {Rooms[i].RoomNr}. {Rooms[i].Description}, has been checked out of.");
+            }
+            else
+            {
+                Console.WriteLine($"Error, Room number: {Rooms[i].RoomNr}. {Rooms[i].Description}, is currently not checked into.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Error! no room with roomnumber '{roomNr}' exists");
+        }
     }
 
-    static void RoomAvaliability()
+    public static void RoomAvaliability()
     {
-
-    }
-    
+        Console.WriteLine("Current room availability");
+        foreach(Room r in Rooms)
+        {
+            if(r.isChecked == false)
+            {
+                Console.WriteLine($"Room number: {r.RoomNr}. {r.Description} is currently available");
+            }
+            else
+            {
+                Console.WriteLine($"Room number: {r.RoomNr}. {r.Description} is currently checked into");
+            }
+        }
+        
+    }  
 }
 
 
